ad8ec7114b84d59bed27da37f05b2b65df3131f1
t-kubrak <chikashikaari@gmail.com>
2016-08-17 13:40:05 -0400

    add special page

---
+++ special_page.php
@@ -0,0 +1,21 @@
+<?php
+include_once "layout_header.php";
+
+print <<<main
+    <div class="col-md-8">
+        <section></section>
+        <section></section>
+    </div>
+
+    <div class="col-md-4" id="sidebar">
+        <aside>
+            <textarea></textarea>
+            <textarea></textarea>
+            <textarea></textarea>
+        </aside>
+    </div>
+
+
+main;
+
+include_once "layout_footer.php";
